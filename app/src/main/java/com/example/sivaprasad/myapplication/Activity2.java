d94bfcff39d220efa1b1d8a3e59c83111c0d6a1f
Siva Nalavenkata <siva002@gmail.com>
2015-05-19 23:21:38 +0530

    moreee

--- app/src/main/java/com/example/sivaprasad/myapplication/Activity2.java
+++ app/src/main/java/com/example/sivaprasad/myapplication/Activity2.java
@@ -1,18 +1,22 @@
 package com.example.sivaprasad.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class Activity2 extends ActionBarActivity {
 
     public final String logtag = this.getClass().getSimpleName();
+    public static String state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        state = "onCreate";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
 
@@ -56,4 +60,10 @@ public class Activity2 extends ActionBarActivity {
         super.onDestroy();
     }
 
+    public void onClickActivity3(View view) {
+
+        Log.d(logtag,"Activity 3 Clicked");
+        Intent intent = new Intent(this,Activity3.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/sivaprasad/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sivaprasad/myapplication/MainActivity.java
@@ -7,47 +7,74 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public final String logtag = this.getClass().getSimpleName();
+    public static String state;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(logtag,"onCreate");
+        Log.d(logtag, "on Create");
         setContentView(R.layout.activity_main);
+        state = "on Create";
+        textView  = (TextView) findViewById(R.id.textview_activity1);
+        textView.setText(state);
+
     }
 
     @Override
     protected void onStart(){
         Log.d(logtag,"onStart");
         super.onStart();
+        state = "on Start";
+        textView  = (TextView) findViewById(R.id.textview_activity1);
+        textView.setText(state);
+
     }
 
     @Override
     protected void onStop(){
         Log.d(logtag,"OnStop");
         super.onStop();
+        state = "on Stop";
+        textView  = (TextView) findViewById(R.id.textview_activity1);
+        textView.setText(state);
     }
 
     @Override
     protected void onResume(){
-        Log.d(logtag, "onResume");
+        Log.d(logtag, "on Resume");
         super.onResume();
+        state = "on Resume";
+        textView  = (TextView) findViewById(R.id.textview_activity1);
+        textView.setText(state);
+
     }
 
     @Override
     protected void onRestart(){
-        Log.d(logtag,"OnRestart");
+        Log.d(logtag,"On Restart");
         super.onRestart();
+        state = "on Re Start";
+        textView  = (TextView) findViewById(R.id.textview_activity1);
+        textView.setText(state);
     }
 
     @Override
     protected void onPause(){
         Log.d(logtag,"OnPause");
         super.onPause();
+        state = "on Pause";
+        textView  = (TextView) findViewById(R.id.textview_activity1);
+        textView.setText(state);
+
     }
 
     @Override
